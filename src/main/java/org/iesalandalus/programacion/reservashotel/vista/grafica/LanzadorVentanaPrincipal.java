591a113e2ba620691fa2da463c5e42a0bf1ad202
alfon <alfonsoalgr@gmail.com>
2024-05-11 01:06:20 +0200

    LanzadorVentanaPrincipal.java

---
+++ src/main/java/org/iesalandalus/programacion/reservashotel/vista/grafica/LanzadorVentanaPrincipal.java
@@ -0,0 +1,51 @@
+package org.iesalandalus.programacion.reservashotel.vista.grafica;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import org.iesalandalus.programacion.reservashotel.vista.grafica.recursos.LocalizadorRecursos;
+
+import java.util.Optional;
+
+public class LanzadorVentanaPrincipal extends Application {
+
+    public void comenzar(){
+        launch();
+    }
+
+    public void start(Stage escenarioPrincipal){
+        try {
+            VBox raiz = FXMLLoader.load(LocalizadorRecursos.class.getResource("vistas/VentanaPrincipal.fxml"));
+
+            Scene scene = new Scene(raiz);
+            escenarioPrincipal.setScene(scene);
+            escenarioPrincipal.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void confirmarSalida(Stage escenarioPrincipal, WindowEvent e){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmar salida");
+        alert.setHeaderText("¿Está seguro de que desea salir?");
+        alert.setContentText("Seleccione una opción.");
+
+        ButtonType bSi = new ButtonType("Sí");
+        ButtonType bNo = new ButtonType("No");
+
+        alert.getButtonTypes().setAll(bSi, bNo);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.isPresent() && result.get() == bSi) {
+            escenarioPrincipal.close();
+        } else {
+            e.consume();
+        }
+    }
+}
