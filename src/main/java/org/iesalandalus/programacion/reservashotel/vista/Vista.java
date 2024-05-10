e1adfdecc0d9a82a5c9df2536b815ce68f53830e
alfon <alfonsoalgr@gmail.com>
2024-05-11 00:08:53 +0200

    Vista abstract

--- src/main/java/org/iesalandalus/programacion/reservashotel/vista/Vista.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/vista/Vista.java
@@ -1,2 +1,15 @@
-package org.iesalandalus.programacion.reservashotel.vista.texto;public class Vista {
+package org.iesalandalus.programacion.reservashotel.vista;
+
+import org.iesalandalus.programacion.reservashotel.controlador.Controlador;
+
+public abstract class Vista {
+    private Controlador controlador;
+
+    public abstract void setControlador(Controlador controlador);
+
+    public abstract Controlador getControlador();
+
+    public abstract void comenzar();
+
+    public abstract void terminar();
 }
